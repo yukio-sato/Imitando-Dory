74ec58b8545f11fbfad8576d850193f044ff7aac
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-03-31 19:55:11 -0300

    [31/03/2023] Inauguração do imitando Dory

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase = Console.ReadLine()!;
+string baleia = frase.ToUpper().Replace("A","aaa").Replace("O","ooo").Replace("E","eee").Replace("I","iii").Replace("U","uuu");
+string traducao =baleia.Substring(0,1).ToUpper()+baleia.Substring(1).ToLower();
+Console.ForegroundColor = ConsoleColor.Blue;
+for (int i = 0; i < traducao.Length; i++)
+{
+ Console.Write(traducao[i]);
+ Thread.Sleep(75);   
+}
+Console.ResetColor();
